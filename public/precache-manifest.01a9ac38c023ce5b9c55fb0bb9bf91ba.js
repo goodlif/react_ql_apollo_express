049f530fd6c64cfef77d2a58cd4b7afba4df36ca
Jonathan George <jonogeorge@gmail.com>
2019-02-13 14:53:20 +0200

    temp app

---
+++ public/precache-manifest.01a9ac38c023ce5b9c55fb0bb9bf91ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "2cb10605e32c232ec223",
+    "url": "/static/js/main.2cb10605.chunk.js"
+  },
+  {
+    "revision": "2f5f4415087b36a4bada",
+    "url": "/static/js/2.2f5f4415.chunk.js"
+  },
+  {
+    "revision": "2cb10605e32c232ec223",
+    "url": "/static/css/main.a9159302.chunk.css"
+  },
+  {
+    "revision": "f4c04dc5ee55351d91968da29ebad842",
+    "url": "/index.html"
+  }
+];
